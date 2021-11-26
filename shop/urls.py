da291cf09be952a009692cf61c41000841e118f6
Navi-n-Don <navi-n-don@ukr.net>
2021-11-27 00:09:59 +0200

    Update cart UI

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('product/', views.product, name='product'),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
-    path('cart', views.cart, name='cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart')
 ]
