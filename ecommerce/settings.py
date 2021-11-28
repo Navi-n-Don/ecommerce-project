2bc752e69d4ee7fcaf094a82cc40e4ed9bd2a5b7
Navi-n-Don <navi-n-don@ukr.net>
2021-11-28 04:11:17 +0200

    Registration form

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Product, Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request, category_slug=None):
@@ -13,6 +15,7 @@ def home(request, category_slug=None):
         products = Product.objects.all().filter(available=True)
     return render(request, 'home.html', {'category':category_page, 'products':products})
 
+
 def product(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
@@ -20,11 +23,13 @@ def product(request, category_slug, product_slug):
         raise e
     return render(request, 'product.html', {'product':product})
 
+
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
     return cart
+
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -44,6 +49,7 @@ def add_cart(request, product_id):
 
     return redirect('cart_detail')
 
+
 def cart_detail(request, total=0, counter=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -57,6 +63,7 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
 
     return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter))
 
+
 def cart_remove(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
@@ -68,9 +75,24 @@ def cart_remove(request, product_id):
         cart_item.delete()
     return redirect('cart_detail')
 
+
 def cart_remove_product(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
     return redirect('cart_detail')
+
+
+def signUpView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            user_group = Group.objects.get(name='User')
+            user_group.user_set.add(signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form':form})
